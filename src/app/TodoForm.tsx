d5cbdcf07b4e44403175399bddf51a6be70550b4
Brandon Garcia <grandonleebarcia@gmail.com>
2024-02-20 08:23:02 -0800

    added CreateTodoForm

--- src/app/TodoForm.tsx
+++ src/app/TodoForm.tsx
@@ -21,6 +21,7 @@ import {
 
 import { prisma } from '../../prisma/client';
 import { createTodoAction } from './actions';
+import { CreateTodoForm } from './create-todo-form';
 
 export function TodoForm() {
   return (
@@ -30,30 +31,9 @@ export function TodoForm() {
         <CardDescription>Keep Track of Your Daily Tasks</CardDescription>
       </CardHeader>
       <CardContent>
-        <form id="TodoForm" action={createTodoAction}>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Todo</Label>
-              <Input id="name" name="content" placeholder="Name of your Todo" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Complete</Label>
-              <Select name="complete">
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="No">No</SelectItem>
-                  <SelectItem value="Yes">Yes</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
+        <CreateTodoForm />
       </CardContent>
-      <CardFooter>
-        <Button form="TodoForm">Add</Button>
-      </CardFooter>
+      <CardFooter></CardFooter>
     </Card>
   );
 }
