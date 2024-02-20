20e0aaf0154dc6fbce607a699bf4080ceb4fc4d6
Brandon Garcia <grandonleebarcia@gmail.com>
2024-02-20 08:21:02 -0800

    added deleteTodoAction to form

--- src/app/TodoList.tsx
+++ src/app/TodoList.tsx
@@ -1,20 +1,27 @@
 import { Button } from '@/components/ui/button';
-import { prisma } from '../../prisma/client';
+import { Checkbox } from '@/components/ui/checkbox';
 import { getAllTodos } from './data-access/todos';
+import { deleteTodoAction } from './actions';
+import { unstable_noStore } from 'next/cache';
 
 export default async function TodoList() {
+  unstable_noStore();
   const todos = await getAllTodos();
 
   return (
     <>
       <ul>
         {todos.map((todo) => (
-          <>
-            <li key={todo.id}>{todo.content}</li>
-            <form>
+          <li
+            key={todo.id}
+            className="flex justify-between items-center gap-2 w-[200px] mb-6"
+          >
+            <Checkbox checked={todo.complete} />
+            <div>{todo.content}</div>
+            <form action={deleteTodoAction.bind(null, todo.id)}>
               <Button>Delete</Button>
             </form>
-          </>
+          </li>
         ))}
       </ul>
     </>
