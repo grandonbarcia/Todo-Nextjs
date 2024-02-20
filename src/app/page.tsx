e3727a393182da6da7b064c8b779045910372206
Brandon Garcia <grandonleebarcia@gmail.com>
2024-02-20 08:20:43 -0800

    add todo list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { TodoForm } from '@/app/TodoForm';
 import Image from 'next/image';
+import TodoList from './TodoList';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-center gap-10 p-24">
       <TodoForm />
+      <TodoList />
     </main>
   );
 }
